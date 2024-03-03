ca792930d2b04d0cca4e6a0d9e7ca49b41c01c58
Pacharapol K. <pcr.pol101@gmail.com>
2024-03-03 21:22:42 +0700

    chap8

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -1,9 +1,49 @@
 const Hotel = require('../models/Hotel.js');
 
 exports.getHotels =async (req,res,next) => {
+    let query;
+    const reqQuery = {...req.query};
+    const removeFields=['select','sort','page','limit'];
+    removeFields.forEach(param=>delete reqQuery[param]);
+    console.log(reqQuery);
+    let queryStr=JSON.stringify(reqQuery);
+    queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=>`$${match}`);
+    query = Hotel.find(JSON.parse(queryStr)).populate('rooms bookings');
+
+    if(req.query.select){
+        const fields=req.query.select.split(',').join(' ');
+        query=query.select(fields);
+    }
+    if(req.query.sort){
+        const sortBy=req.query.sort.split(',').join(' ');
+        query=query.sort(sortBy);
+    }else {
+        query=query.sort('-createdAt');
+    }
+    const page = parseInt(req.query.page,10)||1;
+    const limit = parseInt(req.query.limit,10)||25;
+    const startIndex=(page-1)*limit;
+    const endIndex=page*limit;
+    const total=await Hotel.countDocuments();
+
+    query=query.skip(startIndex).limit(limit);
     try{
-        const hotels = await Hotel.find();
-        res.status(200).json({success: true,count:hotels.length,data:hotels});
+        const hotels = await query;
+        const pagination={}
+        if(endIndex<total){
+            pagination.next={
+                page:page+1,
+                limit
+            }
+        }
+        if(startIndex>0){
+            pagination.prev={
+                page:page-1,
+                limit
+            }
+        }
+        
+        res.status(200).json({success: true,count:hotels.length,pagination,data:hotels});
     }catch(err){
         res.status(400).json({success:false});
     }
@@ -44,10 +84,11 @@ exports.updateHotel = async (req,res,next) => {
 
 exports.deleteHotel = async (req,res,next) => {
     try{
-        const hotel = await Hotel.findByIdAndDelete(req.params.id);
+        const hotel = await Hotel.findById(req.params.id);
         if(!hotel){
             return res.status(400).json({success:false});
          }
+         await hotel.deleteOne();
          res.status(200).json({success:true,data: {}});
     
     }catch(err){
