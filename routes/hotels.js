97659826d21bfac7aee98fb1a31b5bdfbdf9cf35
HuaTanakrit <Tanakit.pitak@gmail.com>
2024-03-03 21:27:08 +0700

    chap9

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const {getHotels,getHotel,createHotel,deleteHotel,updateHotel} = require('../controllers/hotels');
-const { model } = require('mongoose');
+const bookingRouter=require('./bookings');
+const roomRouter=require('./rooms');
+// const { model } = require('mongoose');
 const router = express.Router();
 
-router.route('/').get(getHotels).post(createHotel);
-router.route('/:id').get(getHotel).put(updateHotel).delete(deleteHotel);
+const {protect, authorize} = require('../middleware/auth');
+router.use('/:hotelId/bookings/',bookingRouter);
+router.use('/:hotelId/rooms/',roomRouter);
+
+router.route('/').get(getHotels).post(protect, authorize('admin'), createHotel);
+router.route('/:id').get(getHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
 
 module.exports = router;
