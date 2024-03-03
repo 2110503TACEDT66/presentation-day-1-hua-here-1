f2ab55a0f9567879cbd48a4a947a64f0b9751764
Pacharapol K. <pcr.pol101@gmail.com>
2024-03-03 21:11:36 +0700

    reset repo

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const connectDB=async()=>{
-    mongoose.set('strictQuery',true);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(` Mongoose Connected: ${conn.connection.host}` );
-}
-module.exports = connectDB;
